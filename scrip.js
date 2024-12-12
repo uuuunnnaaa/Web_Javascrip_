a4999c3be9fc2327ebf3aec52b58fdbdfc78d6bf
una0905 <wing42854@gmail.com>
2024-12-12 12:04:41 +0800

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,32 @@
+// 網頁控制台會顯示scrip.js:2 2代表第二行
+console.log("這是外部的JS");
+
+// 變數 variable
+// 儲存網站內的資料
+
+// 變數語法
+// let 變數名稱;
+
+// 變數名稱命名習慣
+// 1.不使用中文
+// 2.不使用數字開頭
+// 3.不使用全形符號 ，,
+// 4.空格 test 1
+// 5.保留字(有作用的關鍵字) if、let
+
+// 建議命名方式:(駝峰式命名)
+// 1.多個單字構成
+// 2.開頭使用小寫
+// 3.單字相連字首用大寫(駝峰式  ex:fontColor)
+// 4.用有意義的名稱命名
+
+let test = 7;
+let fontColor = '#fff';
+
+// 取得變數 get
+console.log(test);
+console.log(fontColor);
+
+// 設定變數 set
+test = 9;
+console.log(test);
